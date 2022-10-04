3037a09302aac103f6bf20619153deef02ac2a5f
Maria <7507577@gmail.com>
2022-10-04 21:32:46 +0300

    All Tests (including Null Array Test)

--- src/WhiteBox.java
+++ src/WhiteBox.java
@@ -1,49 +1,27 @@
 
 public class WhiteBox {
-    // requires: None
-    // modifies: None
-    // effects: None
-    // returns: i such that a[i] == value if value is in a;
-    //	        -1 if value is not in a or a == null
-    // throws: IllegalArgumentException, if a.length != aLength and a != null
-    int find(int[] a, int aLength, int value) {
-        int res = 0, i;
-        if (a == null) {
-            res = -1;
-            a = new int[0];
-        }
-        if (a.length != aLength) {
-            throw new IllegalArgumentException();
-        }
-        for (i = 0; i < aLength; i++)
-            if (a[i] != value)
-                res = -1;
-        return res;
-    }
+	// requires: None
+	// modifies: None
+	// effects: None
+	// returns: i such that a[i] == value if value is in a;
+	//	        -1 if value is not in a or a == null
+	// throws: IllegalArgumentException, if a.length != aLength and a != null
+	int find(int[] a, int aLength, int value)
+	{
+		int res = 0, i;
+		if (a == null) {
+			res = -1;
+			a = new int[0];
+		}
+		if (a.length != aLength) {
+			throw new IllegalArgumentException();
+		}
+		for (i = 0; i < aLength; i++)
+		  if (a[i] != value)
+		    res = -1;
+		return res;
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 /* Corrected version */
 
